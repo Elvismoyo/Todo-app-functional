1388056cf1321da22ce8d77fb9b4f430f0431012
Elvis <elvismoyo1999@gmail.com>
2020-06-06 16:02:01 -0400

    Updates

---
+++ precache-manifest.9ff4448d2bd9440f5d5058e0aa043e89.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a72f431a87312e42ad4fb21e1f85037",
+    "url": "/Todo-app-functional/index.html"
+  },
+  {
+    "revision": "e1eae4f9ca1fbc0c93e6",
+    "url": "/Todo-app-functional/static/css/main.a7e1ae10.chunk.css"
+  },
+  {
+    "revision": "8dc3feec3a987988b92b",
+    "url": "/Todo-app-functional/static/js/2.7336676b.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/Todo-app-functional/static/js/2.7336676b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1eae4f9ca1fbc0c93e6",
+    "url": "/Todo-app-functional/static/js/main.aa3b2a0e.chunk.js"
+  },
+  {
+    "revision": "91475b5b93d6148e1c2c",
+    "url": "/Todo-app-functional/static/js/runtime-main.4e59c320.js"
+  },
+  {
+    "revision": "c6b7ba1dc1c80e00e6c17ac72eb753cc",
+    "url": "/Todo-app-functional/static/media/BlackberryJam.c6b7ba1d.ttf"
+  },
+  {
+    "revision": "f76d5bfcfd888cd5c9c01fc11297ffcc",
+    "url": "/Todo-app-functional/static/media/Recycle-Bin.f76d5bfc.png"
+  },
+  {
+    "revision": "d9cdab3e12bec2438b3369acfe470bc8",
+    "url": "/Todo-app-functional/static/media/pencilback.d9cdab3e.png"
+  },
+  {
+    "revision": "9d40ce02417dd04fe4ed790cc5b5cad3",
+    "url": "/Todo-app-functional/static/media/plus.9d40ce02.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17e30c847a3eb0a210a87f39b5645e14.js"
+  "/Todo-app-functional/precache-manifest.9ff4448d2bd9440f5d5058e0aa043e89.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Todo-app-functional/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
